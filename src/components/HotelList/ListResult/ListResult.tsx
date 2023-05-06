66101f5c121a7c6aaebd55a92141df5cd66126ff
Dmitriy24S <devilins24@gmail.com>
2023-05-06 09:14:27 +0300

    feat: add HotelDetails to HotelPage
    * add HotelDetails component
    * add HotelPage structure & style
    * add navigate to ListResult propety button to details page (hotel page)
    * add var colors to index.css

--- src/components/HotelList/ListResult/ListResult.tsx
+++ src/components/HotelList/ListResult/ListResult.tsx
@@ -1,9 +1,12 @@
+import { useNavigate } from 'react-router-dom'
 import styles from './ListResult.module.scss'
 
 const ListResult = ({ data }: { data: any }) => {
+  const navigate = useNavigate()
+
   return (
     <div className={styles.list}>
-      {data.map((item: any) => (
+      {data.map((item: any, index: number) => (
         <div className={styles.item} key={item.title}>
           <img
             src='https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1'
@@ -34,7 +37,9 @@ const ListResult = ({ data }: { data: any }) => {
               {/* <span className='siTaxOp'>Includes taxes and fees</span> */}
             </div>
           </div>
-          <button className={styles.button}>See availability</button>
+          <button onClick={() => navigate(`/hotels/${index}`)} className={styles.button}>
+            See availability
+          </button>
         </div>
       ))}
     </div>
--- src/pages/HotelPage/HotelPage.tsx
+++ src/pages/HotelPage/HotelPage.tsx
@@ -1,5 +1,22 @@
+import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header'
+import HotelDetails from '../../components/HotelDetails/HotelDetails'
+import MainContentLayout from '../../components/Layout/MainContentLayout'
+import MailList from '../../components/MailList/MailList'
+import Navbar from '../../components/Navbar/Navbar'
+
 const HotelPage = () => {
-  return <div>HotelPage</div>
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <MainContentLayout>
+        <HotelDetails />
+      </MainContentLayout>
+      <MailList />
+      <Footer />
+    </>
+  )
 }
 
 export default HotelPage
