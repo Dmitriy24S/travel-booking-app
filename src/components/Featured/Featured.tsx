18240b99287680b4969cfab52c7537f53a2eeb47
Dmitriy24S <devilins24@gmail.com>
2023-05-06 09:10:30 +0300

    fix: typo & add min-height to img w/ bg-color
    * fix: typo & add min-height to img w/ bg-color in Featured

--- src/components/Featured/Featured.tsx
+++ src/components/Featured/Featured.tsx
@@ -3,7 +3,7 @@ import styles from './Featured.module.scss'
 const featuredItems = [
   {
     title: 'Dublin',
-    description: '123 propertie',
+    description: '123 properties',
     img: 'https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o=',
   },
   {
